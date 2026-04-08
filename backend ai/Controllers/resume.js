101c48fe2a6725e47d50c11aaec9a27f59a71f72
ZEK-XCPY <zaid03297@gmail.com>
2026-04-08 18:01:12 +0530

    Add AI Api and Multer

---
+++ backend ai/Controllers/resume.js
@@ -0,0 +1,82 @@
+const ResumeModel = require("../Models/resume");
+const multer = require("multer");
+const pdfParse = require("pdf-parse");
+const path = require("path");
+const {CohereClient} = require("cohere-ai");
+
+const cohere = new CohereClient({
+  token:"NdVYQfEi6MIytClNqpCKqvBlyir526Ukg2CmHOzp",
+})
+exports.addResume = async (req, res) => {
+  try {
+    const { job_desc, user } = req.body;
+    // console.log(req.file)
+    // console.log(job_desc,user)
+
+    const pdfBuffer = req.file.buffer || null;
+    const pdfPath = req.file.path;
+    const fs = require("fs");
+    const dataBuffer = fs.readFileSync(pdfPath);
+    const pdfData = await pdfParse(dataBuffer);
+
+    const prompt = `
+        You are a resume analyzer. Your task is to analyze the resume and job description and provide a score out of 100 and give a feedback.
+        
+        Resume:${pdfData.text}
+
+        Job Description:${job_desc}
+
+        Return the score and a brief explanation in this format:
+        Score:XX
+        Reason: ...
+
+      `;
+
+    const response = await cohere.chat({
+      model:"command-r-plus-08-2024",
+      message: prompt,
+      maxTokens: 100,
+      temperature: 0.7,
+    })
+
+    let result = response.text;
+    // console.log(result)
+
+    const match = result.match(/Score:\s*(\d+)/);
+    const score = match ? parseInt(match[1],10) : null;
+
+    const reasonMatch = result.match(/Reason:\s*([\s\S]*)/)
+    const reason = reasonMatch ? reasonMatch[1].trim() : null;
+
+    const  newResume = new ResumeModel({
+      user,
+      resume_name:req.file.originalname,
+      job_desc,
+      score,
+      feedback:reason
+    })
+
+    await newResume.save();
+
+    fs.unlinkSync(pdfPath);//remove tep files
+
+    res.status(200).json({
+      message:"Your analysis are ready", data: newResume
+    })
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server Error", message: err.message });
+  }
+};
+
+
+exports.getAllResumeForUser = async (req,res)=>{
+  try{
+
+  }
+  catch(err){
+    console.log(err);
+    return res.status(500).json({error:'Server Error',message:err.message})
+  }
+}
--- backend ai/index.js
+++ backend ai/index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 require('./conn')
 
+app.use(express.json());
+
 const PORT = 4000;
 
-app.get('/', (req, res) => {
-    res.send({
-        message: "Hello from backend"
-    });
-})
-
+const UserRoutes = require("./Routes/user");
+const ResumeRoutes = require('./Routes/resume')
+app.use("/api/user",UserRoutes);
+app.use('/api/resume',ResumeRoutes)
 app.listen(PORT,() =>{
   console.log("backend is running ",PORT)
 })
